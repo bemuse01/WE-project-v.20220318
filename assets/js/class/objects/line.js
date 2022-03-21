b9b602de9361686e78a9006aed86a3b56460667e
bemuse01 <sequeland@gmail.com>
2022-03-21 10:13:58 +0900

    update

---
+++ assets/js/class/objects/line.js
@@ -0,0 +1,59 @@
+import * as THREE from '../../lib/three.module.js'
+
+export default class{
+    constructor({materialOpt}){
+        this.materialOpt = materialOpt
+
+        this.init()
+    }
+
+    // init
+    init(){
+        this.create()
+    }
+
+
+    // create
+    create(){
+        const geometry = this.createGeometry()
+        const material = this.createMaterial()
+        this.mesh = new THREE.Line(geometry, material)
+    }
+    createGeometry(){
+        return new THREE.BufferGeometry()
+    }
+    createMaterial(){
+        if(this.materialOpt.vertexShader){
+            return new THREE.ShaderMaterial(this.materialOpt)
+        }else{
+            return new THREE.LineBasicMaterial(this.materialOpt)
+        }
+    }
+
+
+    // dispose
+    dispose(){
+
+    }
+
+
+    // set
+    setAttribute(name, array, itemSize){
+        this.mesh.geometry.setAttribute(name, new THREE.BufferAttribute(array, itemSize))
+    }
+    setUniform(name, value){
+        this.mesh.material.uniforms[name].value = value
+    }
+
+
+    // get
+    get(){
+        return this.mesh
+    }
+    getGeometry(){
+        return this.mesh.geometry
+    }
+    getMaterial(){
+        return this.mesh.material
+    }
+}
--- assets/js/class/text/build/text.child.build.js
+++ assets/js/class/text/build/text.child.build.js
@@ -1,17 +1,58 @@
+import Line from '../../objects/line.js'
+import L from '../../../data/L.js'
+
 export default class{
-    constructor(){
-        
+    constructor({group}){
+        this.group = group
+
+        this.param = {
+            width: 270 * 0.1,
+            height: 612 * 0.1,
+            color: 0xffffff,
+            size: 1
+        }
+
+        this.init()
     }
 
 
     // init
     init(){
-
+        this.create()
     }
 
 
     // create
     create(){
-        
+        const {position} = this.createAttribute()
+
+        this.object = new Line({
+            materialOpt: {
+                color: this.param.color
+            }
+        })
+
+        this.object.setAttribute('position', new Float32Array(position), 3)
+
+        this.group.add(this.object.get())
+    }
+    createAttribute(){
+        const {width, height} = this.param
+        const position = []
+
+        const wh = width / 2
+        const hh = height / 2
+
+        L.points.forEach((e, i, a) => {
+            const {rx, ry, num} = e
+
+            const x = width * rx - wh
+            const y = height * ry - hh
+
+            if(i !== a.length - 1) position.push(x, -y, 0)
+            else position.push(position[0], position[1], position[2])
+        })
+
+        return {position}
     }
 }
